7fc4ea3d6dab55191a07f0ce45cf4eaaaf479e7c
unknown <almoghazy1@outlook.com>
2018-04-03 20:36:10 +0200

    Client code

---
+++ src/App.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App extends JFrame {
+	private JButton button1;
+	private JTextField textField1;
+	private JPanel panel;
+	private JTextField textField2;
+	private JTextField textField3;
+	private String text1;
+
+	public App() {
+		
+		
+
+		this.getContentPane().setLayout(new GridLayout());
+		button1 = new JButton("Path");
+		textField1 = new JTextField("Item");
+		panel = new JPanel();
+		textField2 = new JTextField("Request");
+		textField3 = new JTextField("Response");
+		text1 = "";
+		add(button1);
+		add(textField1);
+		add(textField2);
+		add(textField3);
+
+
+		button1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				text1 = textField1.getText();
+				System.out.println(textField1.getText());
+			}
+		});
+	}
+	
+	public void start () {
+		JFrame frame = new App();
+		frame.setSize(new Dimension(400,400));
+		frame.setPreferredSize(new Dimension(400,400));
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public JButton getButton1() {
+		return button1;
+	}
+
+	public void setButton1(JButton button1) {
+		this.button1 = button1;
+	}
+
+	public JTextField getTextField1() {
+		return textField1;
+	}
+
+	public void setTextField1(JTextField textField1) {
+		this.textField1 = textField1;
+	}
+
+	public JTextField getTextField2() {
+		return textField2;
+	}
+
+	public void setTextField2(JTextField textField2) {
+		this.textField2 = textField2;
+	}
+
+	public JTextField getTextField3() {
+		return textField3;
+	}
+
+	public void setTextField3(JTextField textField3) {
+		this.textField3 = textField3;
+	}
+
+	public String getText1() {
+		return text1;
+	}
+
+	public void setText1(String text1) {
+		this.text1 = text1;
+	}
+
+	public static void main(String[] args) {
+
+
+
+	}
+}
---
+++ src/Client.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class Client {
+
+
+    public static void main(String args[]) throws IOException {
+
+        App app = new App();
+        app.start();
+        
+        Socket socket = new Socket("127.0.0.1", 1200);
+        String input = null;
+
+        do {
+            input = app.getText1();
+            System.out.print("");
+        } while (input == null);
+
+//        Scanner scanner = new Scanner(input);
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        String msg = "";
+//        msg = scanner.nextLine();
+
+
+        String str = "GET / HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\n" +
+                "Host: 127.0.0.1:1200\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n";
+
+        try {
+        	System.out.println(str);
+            output.writeUTF(str);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        BufferedReader inputMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        System.out.println("2");
+
+        String msgIn = "";
+        try {
+            System.out.println("4");
+
+            while (true) {
+                System.out.println("43");
+                msgIn = inputMessage.readLine();
+                System.out.println(msgIn);
+                System.out.println("42");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("7");
+
+        app.getTextField3().setText(msgIn);
+        System.out.println(msgIn);
+        System.out.println("1");
+
+    }
+}
